1c3a4a44c8b66b3eab7c4853934d032ae92d4560
santinocarrizoo04 <santino.carrizo25@gmail.com>
2025-12-23 14:50:15 -0300

    Estructura base proyecto

---
+++ WebApp SpringBoot - 2026/code/heladeras/src/test/java/com/utn/heladeras/HeladerasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.heladeras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeladerasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
