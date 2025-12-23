1f010a8b3a2a3f436e620482d2813438633d26a2
santinocarrizoo04 <santino.carrizo25@gmail.com>
2025-12-23 14:28:14 -0300

    Estructura base proyecto

---
+++ Webapp Spring - 2025/code/heladeras/src/main/java/com/utn/heladeras/HeladerasApplication.java
@@ -0,0 +1,13 @@
+package com.utn.heladeras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeladerasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeladerasApplication.class, args);
+	}
+
+}
---
+++ Webapp Spring - 2025/code/viandas/src/main/java/com/utn/viandas/ViandasApplication.java
@@ -0,0 +1,13 @@
+package com.utn.viandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViandasApplication.class, args);
+	}
+
+}
