38750467c2907c7bba144816e28fd7c64fa8d69a
santinocarrizoo04 <santino.carrizo25@gmail.com>
2025-12-23 14:25:03 -0300

    Subo estructura base proyecto

---
+++ Webapp Spring - 2025/code/colaboradores/src/test/java/com/utn/colaboradores/ColaboradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.colaboradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaboradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
